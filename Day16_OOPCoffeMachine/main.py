2d663e96ee897a7ae67460a75391091f6cc46ef7
alperergul <alper.ergul.97@gmail.com>
2022-07-20 20:19:54 +0300

    initial

---
+++ Day16_OOPCoffeMachine/main.py
@@ -0,0 +1,13 @@
+# from prettytable import PrettyTable
+# from sympy import pretty
+
+# pretty_table = PrettyTable()
+
+
+# pretty_table.add_column("Pokemon Name", ["Pikachu", "Charmander", "Squirtle"])
+
+# pretty_table.add_column("Type", ["Electric", "Fire", "Water"])
+
+# pretty_table.align = 'l'
+
+# print(pretty_table)
