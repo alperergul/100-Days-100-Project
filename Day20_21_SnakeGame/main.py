824de4314964f192a4ebbad9e03d9962071af737
alperergul <alper.ergul.97@gmail.com>
2022-07-23 21:33:25 +0300

    initial

---
+++ Day20_21_SnakeGame/main.py
@@ -0,0 +1,34 @@
+from hashlib import new
+from tracemalloc import start
+from turtle import Screen
+import time
+from food import Food
+from snake import Snake
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+snake = Snake()
+food = Food()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    snake.move()
+    time.sleep(.1)
+
+
+screen.exitonclick()
